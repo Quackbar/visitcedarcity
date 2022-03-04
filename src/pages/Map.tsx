ec240e53355fe7564b88d3eb9f51f42ffe6a3fb4
Bernard Kintzing <bernardkintzing@gmail.com>
2022-03-04 11:26:36 -0700

    fix map size...

--- src/pages/Map.tsx
+++ src/pages/Map.tsx
@@ -11,13 +11,11 @@ import {
 // @ts-ignore
 import mapboxgl from "!mapbox-gl"; // eslint-disable-line import/no-webpack-loader-syntax
 
-import 'mapbox-gl/dist/mapbox-gl.css'
+import "mapbox-gl/dist/mapbox-gl.css";
 import "../assets/scss/Map.scss";
 
 mapboxgl.accessToken =
   "pk.eyJ1IjoiYmVybmFyZGtpbnR6aW5nIiwiYSI6ImNrenpxc2UwejBjczAzYnMwOXhjeW1zMDEifQ.R_WjPk9TCgHbs-yKfPC1iQ";
-
-
 
 const Map: React.FC = () => {
   const mapContainer = useRef() as React.MutableRefObject<HTMLInputElement>;
@@ -35,16 +33,17 @@ const Map: React.FC = () => {
       center: [-113.061305, 37.683057],
       zoom: 9,
       pitch: 0,
-    })
+    });
 
     map.on("load", () => {
+      map.resize();
       map.addSource("mapbox-dem", {
         type: "raster-dem",
         url: "mapbox://mapbox.mapbox-terrain-dem-v1",
         tileSize: 512,
         maxZoom: 16,
-      })
-      map.setTerrain({ source: "mapbox-dem", exaggeration: 1.5 })
+      });
+      map.setTerrain({ source: "mapbox-dem", exaggeration: 1.5 });
       map.addLayer({
         id: "sky",
         type: "sky",
@@ -53,20 +52,20 @@ const Map: React.FC = () => {
           "sky-atmosphere-sun": [0.0, 90.0],
           "sky-atmosphere-sun-intensity": 15,
         },
-      })
-    })
-  }, [])
+      });
+    });
+  }, []);
 
   return (
     <IonPage id="map-page">
-    <IonContent scrollY={false}>
-    <div
-      id="map"
-      ref={mapContainer}
-      style={{ width: "100vw", height: "100vh" }}
-    />
-    </IonContent>
+      <IonContent scrollY={false}>
+        <div
+          id="map"
+          ref={mapContainer}
+          style={{ width: "100vw", height: "100vh" }}
+        />
+      </IonContent>
     </IonPage>
-  )
-}
+  );
+};
 export default Map;
