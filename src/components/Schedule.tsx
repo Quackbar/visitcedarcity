4f22797a4dc0f597277fd2181ea5aed931223a4c
Quackbar <samluther998@gmail.com>
2022-03-04 15:41:57 -0700

    data for shakespeare

---
+++ src/components/Schedule.tsx
@@ -0,0 +1,17 @@
+import React, { useState, useRef } from 'react';
+
+import { IonToolbar, IonContent, IonPage, IonButtons, IonTitle, IonMenuButton, IonSegment, IonSegmentButton, IonButton, IonIcon, IonSearchbar, IonRefresher, IonRefresherContent, IonToast, IonModal, IonHeader, getConfig } from '@ionic/react';
+
+import { Schedule } from '../models/Schedule';
+
+
+
+
+export default function Schedule() {
+    return (
+        <>
+
+        </>
+
+    );
+}
