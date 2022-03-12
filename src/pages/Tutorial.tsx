b6671b7ed88b4691ccd2ebb923635977f5ebd5bf
Quackbar <samluther998@gmail.com>
2022-03-11 20:25:01 -0700

    Tutorial Survey for early user propogation

--- src/pages/Tutorial.tsx
+++ src/pages/Tutorial.tsx
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
-import { IonContent, IonPage, IonHeader, IonToolbar, IonButtons, IonButton, IonIcon, useIonViewWillEnter } from '@ionic/react';
+import { IonContent, IonPage, IonHeader, IonToolbar, IonButtons, IonButton, IonIcon, useIonViewWillEnter, IonLabel, IonSegment, IonSegmentButton, IonTitle, IonGrid, IonRow } from '@ionic/react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Swiper as SwiperCore } from 'swiper';
-import { arrowForward } from 'ionicons/icons';
+import { arrowForward, heart, thumbsDown, thumbsUp } from 'ionicons/icons';
 import { setHasSeenTutorial } from '../data/user/user.actions';
 import '@ionic/react/css/ionic-swiper.css';
 import { connect } from '../data/connect';
@@ -47,9 +47,197 @@ const Tutorial: React.FC<TutorialProps> = ({ history, setHasSeenTutorial }) => {
       <IonContent fullscreen>
 
         <Swiper onSwiper={setSwiper} onSlideChangeTransitionStart={handleSlideChangeStart}>
-          <SwiperSlide>
-            <img src={img1} alt="" className="slide-image" /><br/>
+          <SwiperSlide >
+            {/*-- Segment with secondary color --*/}
+            <IonGrid class="centered">
+            <IonRow>
+              {/* <IonTitle color="primary"> */}
+              <h1 className="centered">
+                Help Us Get to Know You
+                <br/>
+              </h1>
+              {/* </IonTitle> */}
+            </IonRow>
+            <IonRow>
 
+                <h3 className="centered"><br/>
+                What's Your Favorite Season to Visit?
+              </h3>
+
+              </IonRow>
+              <IonRow>
+              <IonSegment value="hybrid"  onIonChange={e => console.log('Segment selected', e.detail.value)} color="primary">
+                <IonSegmentButton value="standard">
+                  <IonLabel>Summer</IonLabel>
+                </IonSegmentButton>
+                <IonSegmentButton value="hybrid">
+                  <IonLabel>Any</IonLabel>
+                </IonSegmentButton>
+                <IonSegmentButton value="sat">
+                  <IonLabel>Winter</IonLabel>
+                </IonSegmentButton>
+              </IonSegment>
+              </IonRow>
+
+              <IonRow>
+
+                  <h3 className="centered"><br/>
+                  How Do You Feel About Theatre?  
+              </h3>
+
+              </IonRow>
+              <IonRow>
+              <IonSegment value="hybrid" onIonChange={e => console.log('Segment selected', e.detail.value)} color="secondary">
+                <IonSegmentButton value="standard">
+                  <IonLabel>
+                     
+                    
+                    <IonIcon icon={thumbsDown} slot="icon-only" />
+                  </IonLabel>
+                
+                </IonSegmentButton>
+                <IonSegmentButton value="hybrid">
+                <IonLabel>
+                
+                  <IonIcon icon={thumbsUp} slot="icon-only" />
+                </IonLabel>
+                </IonSegmentButton>
+                <IonSegmentButton value="sat">
+                <IonLabel>
+                
+                  <IonIcon icon={heart} slot="icon-only" />
+                </IonLabel>
+                </IonSegmentButton>
+              </IonSegment>
+              </IonRow>
+
+              <IonRow>
+                <h3 className="centered"><br/>
+                              How Do You Feel About the Outdoors?    
+                  </h3>
+                  
+
+              </IonRow>
+              <IonRow>
+              <IonSegment value="hybrid"  onIonChange={e => console.log('Segment selected', e.detail.value)} color="danger">
+                <IonSegmentButton value="standard">
+                  <IonLabel>
+                     
+                    
+                    <IonIcon icon={thumbsDown} slot="icon-only" />
+                  </IonLabel>
+                
+                </IonSegmentButton>
+                <IonSegmentButton value="hybrid">
+                <IonLabel>
+                
+                  <IonIcon icon={thumbsUp} slot="icon-only" />
+                </IonLabel>
+                </IonSegmentButton>
+                <IonSegmentButton value="sat">
+                <IonLabel>
+                
+                  <IonIcon icon={heart} slot="icon-only" />
+                </IonLabel>
+                </IonSegmentButton>
+              </IonSegment>
+              </IonRow>
+
+              <IonRow>
+                <h3 className="centered"><br/>
+                              How Do You Feel About History?    
+                  </h3>
+
+              </IonRow>
+              <IonRow>
+              <IonSegment value="hybrid"  onIonChange={e => console.log('Segment selected', e.detail.value)} color="tertiary">
+                <IonSegmentButton value="standard">
+                  <IonLabel>
+                     
+                    
+                    <IonIcon icon={thumbsDown} slot="icon-only" />
+                  </IonLabel>
+                
+                </IonSegmentButton>
+                <IonSegmentButton value="hybrid">
+                <IonLabel>
+                
+                  <IonIcon icon={thumbsUp} slot="icon-only" />
+                </IonLabel>
+                </IonSegmentButton>
+                <IonSegmentButton value="sat">
+                <IonLabel>
+                
+                  <IonIcon icon={heart} slot="icon-only" />
+                </IonLabel>
+                </IonSegmentButton>
+              </IonSegment>
+              </IonRow>
+              <IonRow >
+              <h3 className="centered"><br/>
+                How do you feel about Arts and Crafts?
+              </h3>
+
+              </IonRow>
+
+              <IonRow>
+
+              <IonSegment value="hybrid"  onIonChange={e => console.log('Segment selected', e.detail.value)} color="success">
+                <IonSegmentButton value="standard">
+                  <IonLabel>
+                     
+                    
+                    <IonIcon icon={thumbsDown} slot="icon-only" />
+                  </IonLabel>
+                
+                </IonSegmentButton>
+                <IonSegmentButton value="hybrid">
+                <IonLabel>
+                
+                  <IonIcon icon={thumbsUp} slot="icon-only" />
+                </IonLabel>
+                </IonSegmentButton>
+                <IonSegmentButton value="sat">
+                <IonLabel>
+                
+                  <IonIcon icon={heart} slot="icon-only" />
+                </IonLabel>
+                </IonSegmentButton>
+              </IonSegment>
+              </IonRow>
+
+
+              <IonRow>
+                <h3 className="centered"><br/>
+                              How Do You Feel About Music?    
+                  </h3>
+
+              </IonRow>
+              <IonRow>
+              <IonSegment value="hybrid"  onIonChange={e => console.log('Segment selected', e.detail.value)} color="warning">
+                <IonSegmentButton value="standard">
+                  <IonLabel>
+                     
+                    
+                    <IonIcon icon={thumbsDown} slot="icon-only" />
+                  </IonLabel>
+                
+                </IonSegmentButton>
+                <IonSegmentButton value="hybrid">
+                <IonLabel>
+                
+                  <IonIcon icon={thumbsUp} slot="icon-only" />
+                </IonLabel>
+                </IonSegmentButton>
+                <IonSegmentButton value="sat">
+                <IonLabel>
+                
+                  <IonIcon icon={heart} slot="icon-only" />
+                </IonLabel>
+                </IonSegmentButton>
+              </IonSegment>
+              </IonRow>
+            </IonGrid>
           </SwiperSlide>
 
           <SwiperSlide>
